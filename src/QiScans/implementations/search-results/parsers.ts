08f814c77df7e28463d08611c6c2b76c88094c8e
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-01-02 14:50:07 -0800

    fix(QiScans): filter titles that are empty or look like urls

--- src/QiScans/implementations/search-results/parsers.ts
+++ src/QiScans/implementations/search-results/parsers.ts
@@ -4,20 +4,33 @@ import { QIScansQueryResponse } from "../shared/models";
 export function parseSearchResults(
     json: QIScansQueryResponse,
 ): SearchResultItem[] {
-    return (json.posts ?? []).map((post) => {
-        const mangaId = post.id.toString();
+    return (json.posts ?? [])
+        .filter((post) => {
+            if (!post.postTitle || post.postTitle.trim().length === 0) {
+                return false;
+            }
+            if (
+                post.postTitle.startsWith("http://") ||
+                post.postTitle.startsWith("https://")
+            ) {
+                return false;
+            }
+            return true;
+        })
+        .map((post) => {
+            const mangaId = post.id.toString();
 
-        let imageUrl = post.featuredImage || "";
-        if (imageUrl.includes("/file/qiscans/")) {
-            imageUrl = imageUrl.replace("/file/qiscans/", "/");
-        }
+            let imageUrl = post.featuredImage || "";
+            if (imageUrl.includes("/file/qiscans/")) {
+                imageUrl = imageUrl.replace("/file/qiscans/", "/");
+            }
 
-        return {
-            mangaId: mangaId,
-            title: Application.decodeHTMLEntities(post.postTitle),
-            imageUrl: imageUrl,
-            subtitle: `${post._count?.chapters ?? 0} Chapters`,
-            contentRating: ContentRating.EVERYONE,
-        };
-    });
+            return {
+                mangaId: mangaId,
+                title: Application.decodeHTMLEntities(post.postTitle),
+                imageUrl: imageUrl,
+                subtitle: `${post._count?.chapters ?? 0} Chapters`,
+                contentRating: ContentRating.EVERYONE,
+            };
+        });
 }
--- src/QiScans/pbconfig.ts
+++ src/QiScans/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 export default {
     name: "QiScans",
     description: "Extension that pulls content from qiscans.org.",
-    version: "1.0.0-alpha.9",
+    version: "1.0.0-alpha.10",
     icon: "icon.png",
     language: "en",
     contentRating: ContentRating.EVERYONE,
